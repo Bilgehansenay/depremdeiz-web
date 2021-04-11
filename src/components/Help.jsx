1055c2509ea37b7e3f2d0f3d54fde2b1e611b8e9
Bilgehan Senay <bilgehannnoe@gmail.com>
2021-04-12 01:24:37 +0300

    yardım oluşturma tamamlandı

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import MyHelps from "./Help/MyHelp"
 import OthersHelp from "./Help/OtherHelp"
 import HelpHeader from "./Help/HelpHeader"
+import CreateHelp from "./Help/CreateHelp/CreateNewHelp"
 
 var database = app.database();
 
@@ -21,13 +22,13 @@ function Help() {
 
 
     return (
-
         <div>
             <Router>
                 <HelpHeader />
                 <Switch>
                     <Route path="/help/myhelps" component={MyHelps} />
                     <Route path="/help/otherhelps" component={OthersHelp} />
+                    <Route path="/help/createHelp" component={CreateHelp} />
                 </Switch>
             </Router>
         </div>
